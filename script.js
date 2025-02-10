2984e9c9102acafd2a847cf7906370b0f52875ed
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2025-02-10 08:59:57 -0800

    Added companions Leo and Frank

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "portion", "artifact"]
+    inventory: ["sword", "portion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }
 };
-//console.log(adventurer);
+// console.log(adventurer);
 
 for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(`Enchanted Item ${i + 1}: ${adventurer.inventory[i]}`);
 }
 
+adventurer.companion.companion = {
+    name: "Frank",
+    type: "Flea",
+    inventory: ["small hat", "sunglasses"]
+}
+console.log(adventurer);
